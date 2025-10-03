35a9e58edc79d1cb29902ffddc887e6bbf1579af
OdinVoyage <voyage4ev@gmail.com>
2025-10-03 11:52:53 +0800

    ui integrate

--- main.js
+++ main.js
@@ -1,53 +1,65 @@
-// const signButton = document.querySelector("#prompt");
-// const log = document.querySelector("#log");
+const result = document.querySelector("#result");
+const log = document.querySelector("#log");
+const logMatch = document.querySelector("#log-match");
+
 let humanScore = 0;
 let computerScore = 0;
-let drawScore = 0;
+let round = 1;
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-  let choice = prompt("Rock Paper or Scissors? : ");
-  return choice ? choice.toLowerCase() : null;
+function getHumanChoice(choice) {
+  return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice = getComputerChoice()) {
+  let message = `\n=== Round ${round} ===\n`;
+
   if (humanChoice === computerChoice) {
-    drawScore++;
-    console.log(`Seri! kamu ${humanChoice} dan bot ${computerChoice} `);
+    message += `Draw! you ${humanChoice} and bot ${computerChoice} \n`;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     humanScore++;
-    console.log(`Player menang! ${humanChoice} mengalahkan ${computerChoice}`);
+    message += `Player won! ${humanChoice} defeat ${computerChoice} \n`;
   } else {
     computerScore++;
-    console.log(`Bot menang! ${computerChoice} mengalahkan ${humanChoice}`);
+    message += `Bot won! ${computerChoice} defeat ${humanChoice} \n`;
   }
+
+  const span = document.getElementById("span-" + round);
+  if (span) {
+    span.textContent = message;
+  }
+  round++;
+
+  if (round > 5) return playGames();
 }
 
 function playGames() {
+  let message = `\n=== Final Result ===`;
+  message += `Player: ${humanScore} | Bot: ${computerScore} `;
+  if (humanScore > computerScore) message += " player won 🎉";
+  else if (computerScore > humanScore) message += " Bot won 🤖";
+  else message += "Score Draw!";
+
+  document.getElementById("log-match").textContent = message;
+
   for (let i = 1; i <= 5; i++) {
-    console.log(`\n=== Ronde ${i} ===`);
-    let humanChoice = getHumanChoice();
-    if (!humanChoice) {
-      console.log("Game dihentikan oleh user.");
-      break;
-    }
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
+    const span = document.getElementById("span-" + i);
+    setTimeout(() => {
+      if (span) span.textContent = "";
+      document.getElementById("log-match").textContent = "";
+    }, 1500);
   }
 
-  console.log(`\n=== HASIL AKHIR ===`);
-  console.log(
-    `Player: ${humanScore} | Bot: ${computerScore} | Draw: ${drawScore}`
-  );
-  if (humanScore > computerScore) console.log("Kamu pemenangnya 🎉");
-  else if (computerScore > humanScore) console.log("Bot pemenangnya 🤖");
-  else console.log("Hasil seri!");
+  round = 1;
+  humanScore = 0;
+  computerScore = 0;
 }
+
